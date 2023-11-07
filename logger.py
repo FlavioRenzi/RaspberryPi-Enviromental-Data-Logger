84fa86a811ac5a9740672e02507779486130aae4
Flavio Renzi <flavioinv@gmail.com>
2023-11-07 16:27:43 +0100

    Added reboot and change time detector

--- logger.py
+++ logger.py
@@ -45,7 +45,9 @@ last_sample = {
     'humidity': 0,
     'presence': 0,
     'scd41_status': 'OK',
-    'pir_status': 'OK'
+    'pir_status': 'OK',
+    'pir_average': 0,
+    'restarted': 'true'
 }
 
 def run_scd41():
@@ -87,18 +89,19 @@ def run_pir(interval=0.5, window=30):
         last_sample['pir_status'] = 'OK'
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(32, GPIO.IN)
-        pir_window = [0]*window
+        pir_window = [0]*int(window/interval)
         index = 0
         while True:
             if GPIO.input(32) == GPIO.HIGH:
                 #print("HIGH")
                 pir_window[index] = 1
-                index = (index + 1) % window
+                index = (index + 1) % len(pir_window)
             else:
                 #print("LOW")
                 pir_window[index] = 0
-                index = (index + 1) % window
+                index = (index + 1) % len(pir_window)
             time.sleep(interval)
+            last_sample['pir_average'] = sum(pir_window)/len(pir_window)
             if sum(pir_window) > 0:
                 last_sample['presence'] = 1
                 pixels.write_one(0, [0, 0, 20])
@@ -109,43 +112,59 @@ def run_pir(interval=0.5, window=30):
         last_sample['pir_status'] = 'ERROR'
         pixels.write_one(1, [20, 0, 0])
         raise e
-        
+
+
 def run_logger(interval):
-    filename = '/home/siemens/RaspberryPi-Enviromental-Data-Logger/data.csv'
+    filename = '/home/siemens/logger/data.csv'
     if not os.path.exists(filename):
         with open(filename, 'w') as f:
-            f.write('timestamp,co2,temperature,humidity,presence,scd41_status,pir_status\n')
+            f.write('timestamp,co2,temperature,humidity,presence,pir_average,scd41_status,pir_status,restarted\n')
     time.sleep(6)
     with open(filename, 'a') as f:
         while True:
-            #
-            f.write('{},{},{},{},{},{},{}\n'.format(
+            new_row = '{},{},{},{},{},{},{},{},{}\n'.format(
                 time.time(),
                 last_sample['co2'],
                 last_sample['temperature'],
                 last_sample['humidity'],
                 last_sample['presence'],
+                last_sample['pir_average'],
                 last_sample['scd41_status'],
-                last_sample['pir_status']
-            ))
+                last_sample['pir_status'],
+                last_sample['restarted']
+            )
+            f.write(new_row)
+            #print(new_row)
             f.flush()
+            last_sample['restarted'] = 'false'
             time.sleep(interval)
 
+def time_checker():
+    last_time = time.time()
+    while True:
+        time.sleep(1)
+        if abs(time.time() - last_time) > 2:
+            print('Change on time detected')
+            last_sample['restarted'] += ' time changed'
+        last_time = time.time()
+        print(time.time())
 
 def signal_handler(sig, frame):
     pixels.off()
     sys.exit(0)
 
 if __name__ == '__main__':
-    
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     t1 = threading.Thread(target=lambda: threadwrap(lambda: run_scd41()), daemon=True)
-    t2 = threading.Thread(target=lambda: threadwrap(lambda: run_pir(interval=0.5, window=30)), daemon=True)
-    t3 = threading.Thread(target=lambda: threadwrap(lambda: run_logger(interval=60*10)), daemon=True)
+    t2 = threading.Thread(target=lambda: threadwrap(lambda: run_pir(interval=0.2, window=30)), daemon=True)
+    t3 = threading.Thread(target=lambda: threadwrap(lambda: run_logger(interval=30)), daemon=True)
+    t4 = threading.Thread(target=lambda: threadwrap(lambda: time_checker()), daemon=True)
     t1.start()
     t2.start()
     t3.start()
+    t4.start()
     t1.join()
     t2.join()
     t3.join()
+    t4.join()
