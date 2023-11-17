7de54bc27b88ece93c0833267ade8d4acd167f94
Flavio Renzi <flavioinv@gmail.com>
2023-11-17 14:17:24 +0100

    changed pir reading and added different windows

--- logger.py
+++ logger.py
@@ -43,17 +43,21 @@ last_sample = {
     'co2': 0,
     'temperature': 0,
     'humidity': 0,
-    'presence': 0,
+    'presence10': 0,
+    'presence30': 0,
+    'presence60': 0,
     'scd41_status': 'OK',
     'pir_status': 'OK',
-    'pir_average': 0,
-    'restarted': 'true'
+    'pir_average10': 0,
+    'pir_average30': 0,
+    'pir_average60': 0,
+    'restarted': 'restart'
 }
 
 def run_scd41():
     try:
+        pixels.write_one(2, [0, 0, 0])
         with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver:
-            pixels.write_one(2, [0, 20, 0])
             last_sample['scd41_status'] = 'OK'
             i2c_connection = I2cConnection(i2c_transceiver)    
             scd41 = Scd4xI2cDevice(i2c_connection)
@@ -83,9 +87,9 @@ def run_scd41():
         raise e
 
 
-def run_pir(interval=0.5, window=30):
+def run_pir(interval=0.5, window=30, presence_field='presence',avg_field='pir_average',led=0):
     try:
-        pixels.write_one(1, [0, 20, 0])
+        pixels.write_one(1, [0, 0, 0])
         last_sample['pir_status'] = 'OK'
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(32, GPIO.IN)
@@ -100,44 +104,51 @@ def run_pir(interval=0.5, window=30):
                 #print("LOW")
                 pir_window[index] = 0
                 index = (index + 1) % len(pir_window)
-            time.sleep(interval)
-            last_sample['pir_average'] = sum(pir_window)/len(pir_window)
-            if sum(pir_window) > 0:
-                last_sample['presence'] = 1
-                pixels.write_one(0, [0, 0, 20])
+            last_sample[avg_field] = sum(pir_window)/len(pir_window)
+            if sum(pir_window) > 1/interval:
+                last_sample[presence_field] = 1
+                if led != -1 : pixels.write_one(led, [0, 0, 15])
             else:
-                last_sample['presence'] = 0
-                pixels.write_one(0, [0, 0, 0])
+                last_sample[presence_field] = 0
+            time.sleep(interval)
     except Exception as e:
         last_sample['pir_status'] = 'ERROR'
         pixels.write_one(1, [20, 0, 0])
         raise e
 
 
-def run_logger(interval):
-    filename = '/home/siemens/RaspberryPi-Enviromental-Data-Logger/data.csv'
-    if not os.path.exists(filename):
-        with open(filename, 'w') as f:
-            f.write('timestamp,co2,temperature,humidity,presence,pir_average,scd41_status,pir_status,restarted\n')
-    time.sleep(6)
-    with open(filename, 'a') as f:
-        while True:
-            new_row = '{},{},{},{},{},{},{},{},{}\n'.format(
-                time.time(),
-                last_sample['co2'],
-                last_sample['temperature'],
-                last_sample['humidity'],
-                last_sample['presence'],
-                last_sample['pir_average'],
-                last_sample['scd41_status'],
-                last_sample['pir_status'],
-                last_sample['restarted']
-            )
-            f.write(new_row)
-            #print(new_row)
-            f.flush()
-            last_sample['restarted'] = 'false'
-            time.sleep(interval)
+def run_logger(interval, filename='/home/siemens/logger/data.csv'):
+    try:
+        pixels.write_one(0, [0, 0, 0])
+        if not os.path.exists(filename):
+            with open(filename, 'w') as f:
+                f.write('timestamp,co2,temperature,humidity,presence10,presence30,presence60,pir_average,scd41_status,pir_status,restarted\n')
+        time.sleep(6)
+        with open(filename, 'a') as f:
+            while True:
+                new_row = '{},{},{},{},{},{},{},{},{},{},{}\n'.format(
+                    time.time(),
+                    last_sample['co2'],
+                    last_sample['temperature'],
+                    last_sample['humidity'],
+                    last_sample['presence10'],
+                    last_sample['presence30'],
+                    last_sample['presence60'],
+                    last_sample['pir_average10'],
+                    last_sample['pir_average30'],
+                    last_sample['pir_average60'],
+                    last_sample['scd41_status'],
+                    last_sample['pir_status'],
+                    last_sample['restarted']
+                )
+                f.write(new_row)
+                #print('10sec: ',last_sample['presence10'], '30sec: ',last_sample['presence30'], '60sec: ',last_sample['presence60'])
+                f.flush()
+                last_sample['restarted'] = 'continue'
+                time.sleep(interval)
+    except Exception as e:
+        pixels.write_one(0, [20, 0, 0])
+        raise e
 
 def time_checker():
     last_time = time.time()
@@ -147,7 +158,7 @@ def time_checker():
             print('Change on time detected')
             last_sample['restarted'] += ' time changed'
         last_time = time.time()
-        print(time.time())
+        #print(time.time())
 
 def signal_handler(sig, frame):
     pixels.off()
@@ -157,14 +168,20 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     t1 = threading.Thread(target=lambda: threadwrap(lambda: run_scd41()), daemon=True)
-    t2 = threading.Thread(target=lambda: threadwrap(lambda: run_pir(interval=0.2, window=30)), daemon=True)
-    t3 = threading.Thread(target=lambda: threadwrap(lambda: run_logger(interval=30)), daemon=True)
+    t2 = threading.Thread(target=lambda: threadwrap(lambda: run_pir(interval=0.2, window=10, presence_field='presence10', avg_field='pir_average10', led=0)), daemon=True)
+    t5 = threading.Thread(target=lambda: threadwrap(lambda: run_pir(interval=0.2, window=30, presence_field='presence30', avg_field='pir_average30', led=1)), daemon=True)
+    t6 = threading.Thread(target=lambda: threadwrap(lambda: run_pir(interval=0.2, window=60, presence_field='presence60', avg_field='pir_average60', led=2)), daemon=True)
+    t3 = threading.Thread(target=lambda: threadwrap(lambda: run_logger(interval=10)), daemon=True)
     t4 = threading.Thread(target=lambda: threadwrap(lambda: time_checker()), daemon=True)
     t1.start()
     t2.start()
     t3.start()
     t4.start()
+    t5.start()
+    t6.start()
     t1.join()
     t2.join()
     t3.join()
     t4.join()
+    t5.join()   
+    t6.join()
